0a9f9ab0fdb7eddd4c613e205d46abf5f16e7bd0
Rashid <mrashidali51@gmail.com>
2023-06-25 21:35:14 -0700

    try to insert data in DB

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const express=require('express');
 require('./DbConnection');
 const mongoose=require('mongoose');
+const itemModel=require("./Models/itemSchema");
 
-const itemctrl=require('./Controllers/itemController');
+// const itemctrl=require('./Controllers/itemController');
 
 const app=express();
 
+app.use(express.json());
+
+app.post('/insert',(req,resp)=>{
+    console.log(req.body);
+    // console.log(Name);
+    const item=new itemModel(req.body);
+    console.log("data in JSon form "+item);
+    item.save();
+    resp.send("data inserted Model");
+});
+// app.get('/get-items',(req,resp)=>{
+//     resp.send("Get items from DB");
+// })
+
+// app.get('/get-all-items',(req,resp)=>{
+//     resp.send("Get All items from DB");
+// });
 
 
-app.use('/item',itemctrl);
+
+// app.use('/item',itemctrl);
 
 
 
