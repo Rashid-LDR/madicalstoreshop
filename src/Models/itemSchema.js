d6787bd556c6a5e444c3ccb237d5e1dc359928fd
Rashid <mrashidali51@gmail.com>
2023-06-25 22:05:19 -0700

    set schema property type

--- src/Models/itemSchema.js
+++ src/Models/itemSchema.js
@@ -2,9 +2,9 @@ const mongoose=require('mongoose');
 
 
 const itemSchema = new mongoose.Schema({
-     item_name: {String},
-     item_price: {Number},
-     item_quantity:{Number}
+     item_name: String,
+     item_price: Number,
+     item_quantity:Number
     });
 
 const itemModel=new mongoose.model("itemModel",itemSchema);
